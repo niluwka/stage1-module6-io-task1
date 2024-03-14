efc6fd3a56e85e7e4139517895d2e099ca450e22
niluwka0403 <ninimamatova0403@gmail.com>
2024-03-14 14:34:42 +0500

    Task IO

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,32 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
 
 
 public class FileReader {
+    public static void main(String[] args) {
 
-    public Profile getDataFromFile(File file) {
+        try(FileInputStream fin=new FileInputStream("/Users/User/IdeaProjects/stage1-module6-io-task1/src/main/resources/Profile.txt"))
+        {
+            int i;
+            while((i=fin.read())!=-1){
+
+                System.out.print((char)i);
+            }
+        }
+        catch(IOException ex){
+
+            System.out.println(ex.getMessage());
+        }
+
+    }
+
+
+
+
+
+    public static Profile getDataFromFile(File file) {
+        System.out.println( file);
         return new Profile();
     }
 }
